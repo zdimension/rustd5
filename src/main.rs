6edbcc9575ec08db8f10ed45d9f78478170a6796
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2022-04-02 19:38:31 +0200

    sample REPL

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,41 @@
 mod tests;
 mod syntax;
 
-fn main()
-{
+use codespan_reporting::diagnostic::{Diagnostic, Label};
+use codespan_reporting::files::SimpleFile;
+use codespan_reporting::term;
+use codespan_reporting::term::termcolor::{ColorChoice, StandardStream};
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
 
+
+fn main() -> anyhow::Result<()> {
+    let writer = StandardStream::stderr(ColorChoice::Always);
+    let config = codespan_reporting::term::Config::default();
+    let mut editor = Editor::<()>::new();
+
+    loop {
+        let line = match editor.readline("> ") {
+            Ok(line) => line,
+            Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => return Ok(()),
+            Err(error) => return Err(error.into()),
+        };
+
+        match syntax::grammar::td5::expr(&line) {
+            Ok(number) => println!("{}", number),
+            Err(error) => {
+                let file = SimpleFile::new("<repl>", line);
+
+                let start = error.location.offset;
+                let diagnostic = Diagnostic::error()
+                    .with_message("parse error")
+                    .with_labels(vec![
+                        Label::primary((), start..start).with_message("parse error")
+                    ])
+                    .with_notes(vec![format!("expected: {}", error.expected)]);
+
+                term::emit(&mut writer.lock(), &config, &file, &diagnostic)?;
+            }
+        }
+    }
 }
-
